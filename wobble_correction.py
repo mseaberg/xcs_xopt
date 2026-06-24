e85786d16132f72a44fab5119686153b7e94c0b6
Matthew H. Seaberg <seaberg@slac.stanford.edu>
2026-06-24 16:59:38 -0700

    removed auto_monitor. Seems to be working as expected with small t1_L move

--- wobble_correction.py
+++ wobble_correction.py
@@ -8,8 +8,8 @@ import time
 # this will have 6 columns, TBD number of rows depending on the L motion resolution we choose
 # t1_L | t1_th2_offset | t1_chi2_offset | t4_L | t4_th2_offset | t4_chi2_offset
 lut = pd.read_csv('wobble_lookup.csv')
-t1_L = EpicsSignalRO('XCS:SND:T1:L.RBV', auto_monitor=True)
-t4_L = EpicsSignalRO('XCS:SND:T4:L.RBV', auto_monitor=True)
+t1_L = EpicsSignalRO('XCS:SND:T1:L.RBV')
+t4_L = EpicsSignalRO('XCS:SND:T4:L.RBV')
 t1_th2_offset = epics.PV('XCS:USER:SND:T1_TH2_OFFSET')
 t1_chi2_offset = epics.PV('XCS:USER:SND:T1_CHI2_OFFSET')
 t4_th2_offset = epics.PV('XCS:USER:SND:T4_TH2_OFFSET')
@@ -26,10 +26,12 @@ t4_chi2_lut = lut['t4_chi2']
 while True:
     # check t1_L and t4_L positions, set offsets to PVs based on lookup table
     # update at ~1Hz (sleep or similar)
-    t1_th2 = np.interp(t1_L.value, t1_L_lut, t1_th2_lut)
-    t1_chi2 = np.interp(t1_L.value, t1_L_lut, t1_chi2_lut)
-    t4_th2 = np.interp(t4_L.value, t4_L_lut, t4_th2_lut)
-    t4_chi2 = np.interp(t4_L.value, t4_L_lut, t4_chi2_lut)
+    t1_L_curr = t1_L.get()
+    t4_L_curr = t4_L.get()
+    t1_th2 = np.interp(t1_L_curr, t1_L_lut, t1_th2_lut)
+    t1_chi2 = np.interp(t1_L_curr, t1_L_lut, t1_chi2_lut)
+    t4_th2 = np.interp(t4_L_curr, t4_L_lut, t4_th2_lut)
+    t4_chi2 = np.interp(t4_L_curr, t4_L_lut, t4_chi2_lut)
     t1_th2_offset.put(t1_th2)
     t1_chi2_offset.put(t1_chi2)
     t4_th2_offset.put(t4_th2)
