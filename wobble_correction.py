256a8886ca243f6c883a184dff2db4c9cfa2e29b
Matt Seaberg <54456229+mseaberg@users.noreply.github.com>
2026-06-24 13:51:37 -0700

    pseudocode for wobble correction

---
+++ wobble_correction.py
@@ -0,0 +1,7 @@
+# load lookup table
+# this will have 6 columns
+# t1_L | t1_th2_offset | t1_chi2_offset | t4_L | t4_th2_offset | t4_chi2_offset
+
+# while True:
+#   check t1_L and t4_L positions, set offsets to PVs based on lookup table
+#   update at ~1Hz (sleep or similar)
